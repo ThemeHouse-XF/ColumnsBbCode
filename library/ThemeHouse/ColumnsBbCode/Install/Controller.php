0ba3ecbf2aafb9658a611cc6ab58df69ab5a408d
Jake B <jake.booher@gmail.com>
2015-11-23 10:52:28 -0600

    Initial Commit

---
+++ library/ThemeHouse/ColumnsBbCode/Install/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class ThemeHouse_ColumnsBbCode_Install_Controller extends ThemeHouse_Install
+{
+
+    protected $_resourceManagerUrl = 'https://xenforo.com/community/resources/columns-bb-code.4147/';
+
+    protected $_minVersionId = 1030000;
+
+    protected $_minVersionString = '1.3.0';
+}
